d08a8a71f1d086b76db3f6df01b04c392d04f439
xasnow <xasnow@163.com>
2019-09-11 00:34:46 +0800

    01，创建一个简单工程。

---
+++ demo-start/src/main/java/com/xasnow/demostart/DemoStartApplication.java
@@ -0,0 +1,13 @@
+package com.xasnow.demostart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoStartApplication.class, args);
+    }
+
+}
---
+++ demo-start/src/main/java/com/xasnow/demostart/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.xasnow.demostart.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring boot";
+    }
+}
